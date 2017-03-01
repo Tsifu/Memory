3577ffe429a3ba5b7b81aae57de6c94c9cb4fcde
Tsi Lung <tsi.lung@gmail.com>
2017-03-01 13:43:47 -0500

    basic setup

---
+++ lib/memory_game.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.getElementById('main');
+  ReactDOM.render(<div>Hello World!</div>, main);
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname,
+  entry: './lib/memory_game.jsx',
+  output: {
+    path: "./",
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
